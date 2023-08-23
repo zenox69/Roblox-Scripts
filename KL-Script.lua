c737149de904c00df31da1d0b44133dc78ee4edb
AishiAiko <80804586+zenox69@users.noreply.github.com>
2023-08-23 09:54:56 +0800

    Update KL-Script.lua
    SHITTY SERVERHOP UPDATE

--- KL-Script.lua
+++ KL-Script.lua
@@ -342,29 +342,33 @@ function serverHop()
         Text = 'Warping',
         Duration = 5
     })
-    local Player = game.Players.LocalPlayer    
-    local Http = game:GetService("HttpService")
-    local TPS = game:GetService("TeleportService")
-    local Api = "https://games.roblox.com/v1/games/"
-
-    local _place,_id = game.PlaceId, game.JobId
-    local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
-    function ListServers(cursor)
-    local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-    return Http:JSONDecode(Raw)
-    end
-
-    local Next; repeat
-    local Servers = ListServers(Next)
-    for i,v in next, Servers.data do
-        if v.playing < v.maxPlayers and v.id ~= _id then
-            local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
-            if s then break end
-        end
-    end
     
-    Next = Servers.nextPageCursor
-    until not Next
+    local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/zenox69/Roblox-Scripts/main/serverhop.lua")()
+
+    module:Teleport(6381829480)
+    -- local Player = game.Players.LocalPlayer    
+    -- local Http = game:GetService("HttpService")
+    -- local TPS = game:GetService("TeleportService")
+    -- local Api = "https://games.roblox.com/v1/games/"
+
+    -- local _place,_id = game.PlaceId, game.JobId
+    -- local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+    -- function ListServers(cursor)
+    -- local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+    -- return Http:JSONDecode(Raw)
+    -- end
+
+    -- local Next; repeat
+    -- local Servers = ListServers(Next)
+    -- for i,v in next, Servers.data do
+    --     if v.playing < v.maxPlayers and v.id ~= _id then
+    --         local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+    --         if s then break end
+    --     end
+    -- end
+    
+    -- Next = Servers.nextPageCursor
+    -- until not Next
 end
 
 function serverHopLowest()
